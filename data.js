11253c0bf4598734bf6a7ddbdef812aa4c9bdeac
Marcelo Barbosa <mbarbosa@gmail.com>
2019-04-08 00:09:40 -0300

    let the hacking begin

---
+++ data.js
@@ -0,0 +1,46 @@
+const products = [
+  {
+    name: 'Monitor Samsung Curvo de 34" C34F791WQL',
+    url: 'https://www.samsung.com/br/monitors/ultra-wide-c34f791/',
+    imageUrl: 'https://images.samsung.com/is/image/samsung/br-ultra-wide-c34f791-lc34f791wqlxzd-dynamicwhite-72942896?$PD_GALLERY_L_JPG$',
+    originalPrice: 4499.1,
+    price: 2499,
+    details: [
+      '9 meses de garantia Samsung',
+      '3 meses de uso',
+      'todos os manuais e acessórios',
+    ]
+  },
+  {
+    name: 'Cadeira Herman Miller Sayl Preta',
+    url: 'https://store.hermanmiller.com.br/cadeira-sayl-preta-as2-0002-preto/p',
+    imageUrl: 'https://hermanmiller.vteximg.com.br/arquivos/ids/156102/AS2VA22HAN2C7_011.jpg?v=636480937955030000',
+    originalPrice: 4410,
+    price: 2499,
+    details: [
+      '7 anos de garantia Herman Miller',
+      'sim, 7 ANOS',
+    ]
+  },
+  {
+    name: 'iPhone XS Space Grey 64GB',
+    url: 'https://www.apple.com/br/shop/buy-iphone/iphone-xs/tela-de-5,8-polegadas-64gb-cinza-espacial',
+    imageUrl: 'https://i.expansys.net/img/p/313672/apple-iphone-xs-a1920.jpg',
+    originalPrice: 6569.1,
+    price: 4200,
+    details: [
+      '7 meses de garantia Apple',
+    ]
+  },
+  {
+    name: 'Refrigerador Samsung RT5000K Twin Cooling Plus, 384 L',
+    url: 'https://www.samsung.com/br/support/model/RT38K5430SL/AZ/',
+    imageUrl: 'https://images.samsung.com/is/image/samsung/br-top-mount-freezer-rt38k5430sl-az-rt38k5430sl-az-001-front-silver?$L2-Thumbnail$',
+    originalPrice: 1839.20,
+    price: 1199,
+    details: [
+      'menos de 2 anos de uso',
+      'baixo consumo de energia',
+    ]
+  },
+]
