4b62f5c5d99968e557c19872c491aeb43976b0a1
Marcelo Barbosa <mbarbosa@gmail.com>
2019-04-10 15:06:02 -0300

    add products

--- data.js
+++ data.js
@@ -27,7 +27,7 @@ const products = [
     url: 'https://www.apple.com/br/shop/buy-iphone/iphone-xs/tela-de-5,8-polegadas-64gb-cinza-espacial',
     imageUrl: 'https://i.expansys.net/img/p/313672/apple-iphone-xs-a1920.jpg',
     originalPrice: 6569.1,
-    price: 4200,
+    price: 4199,
     details: [
       '7 meses de garantia Apple',
     ]
@@ -41,6 +41,60 @@ const products = [
     details: [
       'menos de 2 anos de uso',
       'baixo consumo de energia',
+    ]
+  },
+  {
+    name: 'Ar-Condicionado Samsung Split Digital Inverter Frio (220 V)',
+    url: 'https://www.samsung.com/br/air-conditioners/ar-condicionado-split-digital-inverter-frio-2017/AR12KVSPBGMNAZ/',
+    imageUrl: 'https://images.samsung.com/is/image/samsung/br-ar-condicionado-split-digital-inverter-frio-2017-ar12kvspbgmnaz-frontpmgray-69147213?$PD_GALLERY_L_JPG$',
+    originalPrice: 1568.94,
+    price: 899,
+    details: [
+      'menos de 2 anos de uso',
+    ]
+  },
+  {
+    name: 'TV Samsung KU6450 49” UHD 4K',
+    url: 'https://www.samsung.com/br/tvs/uhd-ku6450/',
+    imageUrl: 'https://images.samsung.com/is/image/samsung/br-uhd-ku6450-un49ku6450gxzd-frontblack-81752123?$PD_GALLERY_L_JPG$',
+    originalPrice: 2499,
+    price: 1499,
+    details: [
+      '4K de Verdade com HDR Premium',
+      'Controle Remoto Único',
+    ]
+  },
+  {
+    name: 'Fogão 5 Bocas Electrolux Duplo Forno Grill 76DTX',
+    url: 'https://www.buscape.com.br/electrolux-celebrate-76dtx-piso-5-bocas-prata',
+    imageUrl: 'http://www.gazin.com.br/comprar/produtos/fotos900/14910.jpg',
+    originalPrice: 2499,
+    price: 999,
+    details: [
+      '6 anos de uso',
+    ]
+  },
+  {
+    name: 'Volvo C30 T5 2.5 Automático 2009',
+    url: 'https://www.volvocars.com/br/sobre/nossa-empresa/heranca-volvo/modelos-historicos/69-c30',
+    imageUrl: 'img/volvo-c30.jpg',
+    originalPrice: 39000,
+    price: 39000,
+    details: [
+      '2 anos de garantia contra corrosão da carroceria',
+      '72.000 km',
+      '230 cavalos',
+    ]
+  },
+  {
+    name: 'Lava & Seca Samsung Seine, 10.1 kg (110 V)',
+    url: 'https://www.samsung.com/br/support/model/WD106UHSAWQ/AZ/',
+    imageUrl: 'https://www.pontofrio-imagens.com.br/Control/ArquivoExibir.aspx?IdArquivo=8192408',
+    originalPrice: 2564,
+    price: 1199,
+    details: [
+      'menos de 2 anos de uso',
+      'WD106UHSAWQ',
     ]
   },
 ]
