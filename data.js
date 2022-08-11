3dae5c073d5a5c6442dd435b217653483f817310
Julian Alarcon <alarconj@gmail.com>
2022-08-10 23:05:12 -0500

    Added items

--- data.js
+++ data.js
@@ -1,195 +1,246 @@
 const products = [
   {
-    name: 'Monitor Samsung Curvo de 34" C34F791WQL',
-    url: 'https://www.samsung.com/br/monitors/ultra-wide-c34f791/',
-    imageUrl: 'img/sofa.jpg',
-    price: 2499,
+    name: 'Xiaomi Mi Body Composition Scale 2',
+    url: 'https://www.mi.com/mx/product/mi-body-composition-scale-2/',
+    imageUrl: 'img/xiaomi-scale.png',
+    originalPrice: 70000,
+    price: 50000,
     details: [
-      '9 meses de garantia Samsung',
-      '3 meses de uso',
-      'todos os manuais e acessórios',
-    ],
-    sold: false
-  },
-  {
-    name: 'Cadeira Herman Miller Sayl Preta',
-    url: 'https://store.hermanmiller.com.br/cadeira-sayl-preta-as2-0002-preto/p',
-    imageUrl: 'https://hermanmiller.vteximg.com.br/arquivos/ids/158889/cadeira_sayl_preta_AS2VA22HA-0002_1.jpg?v=636905245002930000',
-    price: 2499,
-    details: [
-      '7 anos de garantia Herman Miller',
-      'sim, 7 ANOS',
-    ],
-    sold: false
-  },
-  {
-    name: 'iPhone XS Space Grey 64GB',
-    url: 'https://www.apple.com/br/shop/buy-iphone/iphone-xs/tela-de-5,8-polegadas-64gb-cinza-espacial',
-    imageUrl: 'https://i.expansys.net/img/p/313672/apple-iphone-xs-a1920.jpg',
-    originalPrice: 5455.99,
-    price: 3999,
-    details: [
-      '7 meses de garantia Apple',
-    ],
-    sold: false
-  },
-  {
-    name: 'Mesa de Jantar com 4 Cadeiras',
-    url: '',
-    imageUrl: 'img/mesa-jantar.jpg',
-    originalPrice: 1800,
-    price: 1800,
-    details: [
-      'Tampo de Vidro',
-      'Tamanho: 1,40 x 1,40',
-    ],
-    sold: false
-  },
-  {
-    name: 'Sofá 4 Lugares Chase Retrátil',
-    url: '',
-    imageUrl: 'img/sofa.jpg',
-    originalPrice: 1800,
-    price: 1800,
-    details: [
-      'Tamanho: 2,80 x 1,15',
-      'Chase aberto: 1,80',
-    ],
-    sold: false
-  },
-  {
-    name: 'Refrigerador Samsung RT5000K Twin Cooling Plus, 384 L',
-    url: 'https://www.samsung.com/br/support/model/RT38K5430SL/AZ/',
-    imageUrl: 'https://images.samsung.com/is/image/samsung/br-top-mount-freezer-rt38k5430sl-az-rt38k5430sl-az-001-front-silver?$L2-Thumbnail$',
-    originalPrice: 1839.20,
-    price: 1199,
-    details: [
-      'menos de 2 anos de uso',
-      'baixo consumo de energia',
-    ],
-    sold: false
-  },
-  {
-    name: 'Ar-Condicionado Samsung Split Digital Inverter Frio (220 V)',
-    url: 'https://www.samsung.com/br/air-conditioners/ar-condicionado-split-digital-inverter-frio-2017/AR12KVSPBGMNAZ/',
-    imageUrl: 'https://images.samsung.com/is/image/samsung/br-ar-condicionado-split-digital-inverter-frio-2017-ar12kvspbgmnaz-frontpmgray-69147213?$PD_GALLERY_L_JPG$',
-    originalPrice: 1568.94,
-    price: 899,
-    details: [
-      'menos de 2 anos de uso',
-      '12,000 BTU/h',
-    ],
-    sold: false
-  },
-  {
-    name: 'TV Samsung KU6450 49” UHD 4K',
-    url: 'https://www.samsung.com/br/tvs/uhd-ku6450/',
-    imageUrl: 'https://images.samsung.com/is/image/samsung/br-uhd-ku6450-un49ku6450gxzd-frontblack-81752123?$PD_GALLERY_L_JPG$',
-    originalPrice: 2499,
-    price: 1499,
-    details: [
-      '4K de verdade com HDR Premium',
-      'controle remoto único',
-    ],
-    sold: true
-  },
-  {
-    name: 'Fogão 5 Bocas Electrolux Duplo Forno Grill 76DTX',
-    url: 'https://www.buscape.com.br/electrolux-celebrate-76dtx-piso-5-bocas-prata',
-    imageUrl: 'http://www.gazin.com.br/comprar/produtos/fotos900/14910.jpg',
-    originalPrice: 2499,
-    price: 999,
-    details: [
-      '6 anos de uso',
-    ],
-    sold: true
-  },
-  {
-    name: 'Volvo C30 T5 2.5 Automático 2009',
-    url: 'https://www.volvocars.com/br/sobre/nossa-empresa/heranca-volvo/modelos-historicos/69-c30',
-    imageUrl: 'img/volvo-c30.jpg',
-    originalPrice: 39000,
-    price: 39000,
-    details: [
-      '2 anos de garantia contra corrosão da carroceria',
-      '72.000 km',
-      '230 cavalos',
-    ],
-    sold: true
-  },
-  {
-    name: 'Lava & Seca Samsung Seine, 10.1 kg (110 V)',
-    url: 'https://www.samsung.com/br/support/model/WD106UHSAWQ/AZ/',
-    imageUrl: 'https://www.pontofrio-imagens.com.br/Control/ArquivoExibir.aspx?IdArquivo=8192408',
-    originalPrice: 2564,
-    price: 1199,
-    details: [
-      'menos de 2 anos de uso',
-      'WD106UHSAWQ',
-    ],
-    sold: true
-  },
-  {
-    name: 'iPhone 8 Gold 64GB',
-    url: 'https://www.apple.com/br/shop/buy-iphone/iphone-8/tela-de-4,7-polegadas-64gb-dourado',
-    imageUrl: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone8/gold/iphone8-gold-select-2018?wid=940&hei=1112&fmt=png-alpha&qlt=80&.v=1550795416637',
-    originalPrice: 2991.12,
-    price: 2499,
-    details: [
-      'sem 1 arranhão',
-      'sempre usado com película e capa',
-      'funcionando perfeitamente :)'
-    ],
-    sold: true
-  },
-  {
-    name: 'Espelho Adnet',
-    url: '',
-    imageUrl: 'https://cdn.leroymerlin.com.br/products/espelho_decorativo_adnet_branco_com_alca_caramelo_60x40cm_89664071_0001_600x600.jpg',
-    originalPrice: 350,
-    price: 250,
-    details: [
-      'Menos de 1 ano de uso',
-      'A: 60cm, L: 40cm, P: 4cm',
-      'Diâmetro: 40cm',
-    ],
-    sold: true
-  },
-  {
-    name: 'Cadeira Charles Eames Eiffel Base Madeira',
-    // url: '',
-    imageUrl: 'https://images-americanas.b2w.io/produtos/01/00/oferta/32302/5/32302522_3GG.jpg',
-    originalPrice: 158,
-    price: 100,
-    details: [
-      'Menos de 1 ano de uso',
-      'Cor: branca',
-    ],
-    reserved: true
-  },
-  {
-    name: 'Estante Hipster Grandona Mel',
-    url: 'https://www.tadah.com.br/moveis/estante-hipster-grandona-mel.html',
-    imageUrl: 'https://images-americanas.b2w.io/produtos/01/00/oferta/32302/5/32302522_3GG.jpg',
-    originalPrice: 167.9,
-    price: 100,
-    details: [
-      'Tamanho: 170 x 65 x 30 cm',
-      'Madeira Pinus de Reflorestamento',
+      'ENTREGA INMEDIATA',
+      'Bascula digital Xiaomi',
+      'Se integra con application de Smartphone. Peso, agua, porcentaje grasa, masa muscular/osea',
+      'Como nueva!',
     ],
     available: true
   },
   {
-    name: 'Base Baú Ortobom Queen',
-    url: 'https://www.ortobom.com.br/cama/base-bau-ortobom-nobuck-cream/queen158',
-    imageUrl: 'https://www.ortobom.com.br/Content/Photo?id=2307&w=770&h=770',
-    originalPrice: 1999,
-    price: 1200,
+    name: 'Horno Microondas Samsung AGE83X',
+    url: 'https://www.samsung.com/co/microwave-ovens/grill/microwave-oven-grill-age83x/',
+    imageUrl: 'img/microondas.jpg',
+    originalPrice: 459900,
+    price: 200000,
     details: [
-      'Tamanho: Queen',
-      'Forração em Couro Nobuck creme',
-      'Sistema de fechamento e abertura com pistões pneumáticos (amortecedores) que facilitam o manuseio',
+      'ENTREGA 02 de Septiembre',
+      'Como nuevo!',
     ],
-    sold: true
+    // sold: false
+    available: true
+  },
+  {
+    name: 'Mueble Comoda',
+    url: 'img/comoda-collage.jpg',
+    imageUrl: 'img/comoda.jpg',
+    price: 80000,
+    details: [
+      'ENTREGA 02 de Septiembre',
+      'Comoda cafe con 3 cajones',
+      '80 cm ancho x 75 cm alto x 38 cm profundo',
+      'Buen estado!',
+    ],
+    // sold: false
+    available: true
+  },
+  {
+    name: 'Smart TV Samsung UN48JU6100 4K 48 pulgadas',
+    url: 'https://www.linio.com.co/p/televisor-samsung-48-4k-un48ju6100-led-negro-tpq4wk',
+    imageUrl: 'img/tv.jpg',
+    originalPrice: 1899900,
+    price: 1200000,
+    details: [
+      'ENTREGA 02 de Septiembre',
+      'Netflix/YouTube/PrimeVideo/Spotify',
+      '4K, 3 puertos HDMI, Internet Wifi, TDT, Incluye control remoto',
+      'Buen estado!',
+    ],
+    // sold: false
+    available: true
+  },
+  {
+    name: 'Rascador gatos 3 niveles',
+    imageUrl: 'img/rascador.jpg',
+    price: 90000,
+    details: [
+      'ENTREGA 02 de Septiembre',
+      'Rascador amarillo/negro',
+      'Altura 165 cm',
+      'Poco uso',
+    ],
+    // sold: false
+    available: true
+  },
+  {
+    name: 'Fuente flor de agua para gatos Catit (mas repuesto) original',
+    url: 'https://www.catit.com/products/drinking-fountains/flower-fountains/#original-flower-fountain',
+    imageUrl: 'img/fuente.jpg',
+    originalPrice: 119000,
+    price: 45000,
+    details: [
+      'ENTREGA 10 de Septiembre',
+      'Menos de 2 años de uso',
+      'Raspada pero 100% funcional',
+    ],
+    // sold: false
+    available: true
+  },
+  {
+    name: 'Nevera Whirlpool WRW25AKTWW 257 Litros',
+    url: 'https://www.whirlpool.com.co/nevera-top-mount-9-pies-cubicos-wrw25bktww/p',
+    imageUrl: 'img/nevera.jpg',
+    originalPrice: 1549000,
+    price: 750000,
+    details: [
+      'ENTREGA 02 de Septiembre',
+      'Congelador no frost, modalidad ahorradora',
+      'Algunos arañazos menores, 100 funcional',
+    ],
+    // sold: false
+    available: true
+  },
+  {
+    name: 'Lavadora Secadora Samsung WD11M44733S 11.5 Kg',
+    url: 'https://www.youtube.com/watch?v=KWxgPi7W1TI',
+    imageUrl: 'img/lavadora.jpg',
+    originalPrice: 2050000,
+    price: 1700000,
+    details: [
+      'ENTREGA 02 de Septiembre',
+      'Lavadora secadora (electrica)',
+      '60 cm ancho x 85 cm alto x 65 cm profundo',
+      'Como nueva! Menos de 1 año de uso',
+    ],
+    // sold: false
+    available: true
+  },
+  {
+    name: 'Tapete Gris',
+    url: 'img/tapete-collage.jpg',
+    imageUrl: 'img/tapete.jpg',
+    originalPrice: 300000,
+    price: 150000,
+    details: [
+      'ENTREGA 02 de Septiembre',
+      'Tapete gris con franjas hecho en Turquia',
+      'Como nuevo!',
+    ],
+    // sold: false
+    available: true
+  },
+  {
+    name: 'Sofacama verde tela antiarañazos!',
+    url: 'img/sofa-collage.jpg',
+    imageUrl: 'img/sofa.jpg',
+    originalPrice: 1400000,
+    price: 1000000,
+    details: [
+      'ENTREGA 02 de Septiembre',
+      'Realmente funciona! Tenemos 2 gatos y esta sin 1 rasguño!',
+      '3 posiciones, bastante comodo. Producto colombiano',
+      'Sofa: 182 cm ancho x 95 cm alto x 94 cm profundo',
+      'Cama: 182 cm ancho x 60 cm alto x 65 cm profundo',
+      'Como nuevo!',
+    ],
+    // sold: false
+    available: true
+  },
+  {
+    name: 'Licuadora/Extractor Nutribullet Pro Original 900W',
+    url: 'https://nutribulletcolombia.com/product/nutribullet-900-watts/',
+    imageUrl: 'img/nutribullet.jpg',
+    originalPrice: 320000,
+    price: 250000,
+    details: [
+      'ENTREGA INMEDIATA',
+      'Con todos los accesorios, en caja original',
+      'Como nuevo!',
+    ],
+    // sold: false
+    available: true
+  },
+  {
+    name: 'Juego de Alcoba, cama doble y mesas de noche',
+    url: 'img/cama-collage.jpg',
+    imageUrl: 'img/cama.jpg',
+    price: 1200000,
+    details: [
+      'ENTREGA 02 de Septiembre',
+      '1 cama doble, 2 mesas de noche',
+      'Se entrega con Colchon Ortopedico Paraiso Ortholife , 1 plumon, 2 Duvet',
+      'Como nuevo!',
+    ],
+    // sold: false
+    available: true
+  },
+  {
+    name: 'Bicicleta Todoterreno GW negro con gris',
+    url: 'img/bicicleta-gris-collage.jpg',
+    imageUrl: 'img/bicicleta-gris.jpg',
+    originalPrice: 900000,
+    price: 700000,
+    details: [
+      'ENTREGA INMEDIATA',
+      'Bicicleta cambios Shimano, frenos de disco, amortiguador frontal',
+      'Incluye casco!',
+      'RIN 29',
+      'Como nueva!',
+    ],
+    // sold: true
+    available: true
+  },
+  {
+    name: 'Bicicleta Todoterreno GW negro con verde',
+    url: 'img/bicicleta-gris-collage.jpg',
+    imageUrl: 'img/bicicleta-gris.jpg',
+    originalPrice: 900000,
+    price: 700000,
+    details: [
+      'ENTREGA INMEDIATA',
+      'Bicicleta cambios Shimano, frenos de disco, amortiguador frontal',
+      'Incluye casco!',
+      'RIN 29',
+      'Como nueva!',
+    ],
+    // sold: true
+    available: true
+  },
+  {
+    name: 'Parlante Google Home',
+    url: 'https://www.xataka.com/basics/todo-que-puedes-hacer-google-home-como-configurarlo-reyes-te-han-traido-uno',
+    imageUrl: 'img/google-home.jpg',
+    originalPrice: 350000,
+    price: 150000,
+    details: [
+      'ENTREGA INMEDIATA',
+      'Compatible con Google Assistant, comandos en Español e Ingles',
+      'Como nuevo!',
+    ],
+    // sold: false,
+    reserved: true,
+  },
+  {
+    name: 'Parlante Google Home Mini Gris',
+    url: 'https://www.xataka.com/accesorios/google-home-mini-mas-pequeno-pero-con-las-mismas-prestaciones-que-su-hermano-mayor',
+    imageUrl: 'img/google-home-mini.jpg',
+    originalPrice: 214000,
+    price: 70000,
+    details: [
+      'ENTREGA INMEDIATA',
+      'Compatible con Google Assistant, comandos en Español e Ingles',
+      'Como nuevo!',
+    ],
+    available: true
+  },
+  {
+    name: 'Aspiradora Robot iRobot Roomba 675',
+    url: 'https://www.irobotcolombia.com/producto/roomba-675/',
+    imageUrl: 'img/roomba.jpg',
+    originalPrice: 1270000,
+    price: 700000,
+    details: [
+      'ENTREGA INMEDIATA',
+      'Compatible con Google Assistant, programacion desde App',
+      'Buen estado!',
+    ],
+    // sold: false
+    available: true
   },
 ]
